2b222c0c5a674d6cbfe6bf10e95cd8f01384f8eb
Harriom98 <hariomcisco2@gmail.com>
2024-05-31 19:11:30 +0000

    Data Types.

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,38 @@
+"use strict"; // treat all js code as newer version.
+
+// alert(3 + 3) // we are using node.js , not browser.
+
+console.log(3 + 3); console.log("Hariom") // code readability is not good.
+
+// code redability should be high.
+
+console.log(3 + 3)
+console.log("Hariom")
+
+let name = "Hariom"
+let age = 19
+let isLoggedIn = false
+let state = null
+
+
+// primitive data types.
+
+// number =>  2 to power 53.
+// bigInt 
+// string => "" , ''
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness find.
+
+
+// object
+
+console.log(typeof 'hariom')  // string
+console.log(typeof age)  // number
+console.log(typeof null) // object
+console.log(typeof undefined)  // undefined.
+
+
+
+
